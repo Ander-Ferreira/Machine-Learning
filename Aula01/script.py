742fb987b66d957c67e7171d3ed3ad19fa0304d9
Ander-ferreira <andersonadm1314@gmail.com>
2025-03-12 21:43:05 -0300

    commitando aula

---
+++ Aula01/script.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+
+#Cria um dataframe
+marketing = pd.read_csv('marketing.csv', delimiter =',')
+
+
+print(marketing.head(5))
+
+marketing.describe(include='all')
+
+print(marketing['converted'].head(5))
+
+#Estou selecionando a coluna 'converted' e sobrescrevendo com formato booleano
+marketing['converted'] = marketing['converted'].astype('bool')
+
+#marketing=['is_house_ads'] = np.where(marketing['marketing_channel'] == 'House Ads', True, False)
+
+#fillna('') atribui valores aos espaços vazios da tabela
+
+
+
+
+
+
+
+
+
+
